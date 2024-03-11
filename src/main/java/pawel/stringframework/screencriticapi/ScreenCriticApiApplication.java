9b4921f714da1c3dcff78e6106c99a0df576571a
Pawel Styka <pawel@styka.pl>
2024-03-11 19:30:40 +0100

    first commit

---
+++ src/main/java/pawel/stringframework/screencriticapi/ScreenCriticApiApplication.java
@@ -0,0 +1,13 @@
+package pawel.stringframework.screencriticapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenCriticApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenCriticApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pawel/stringframework/screencriticapi/ScreenCriticApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pawel.stringframework.screencriticapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenCriticApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
